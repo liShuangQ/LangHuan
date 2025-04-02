d6df2f6bcc234c79c3b650fedaa152f32279d096
shuangqi.li <li_shuangq@foxmail.com>
2025-04-02 17:23:29 +0800

    feat(aimodel): 更新 OneAPI 配置并优化模型选项获取逻辑
    - 在 .env 文件中更新了 OneAPI 相关配置，启用 OneAPI 模型获取
    - 优化了 aimodel.ts 中的 setModelOptions 方法，根据 OneAPI 配置动态选择模型获取方式
    - 移除了不必要的注释代码，提高了代码可读性和维护性

--- langhuan-web/src/store/aimodel.ts
+++ langhuan-web/src/store/aimodel.ts
@@ -10,53 +10,53 @@ export default defineStore("aimodel", {
     actions: {
         async setModelOptions() {
             const oneapi = process.env.IS_ONEAPI_GET_MODEL as string;
-            // oneapi === "false" &&
-            //     (await http
-            //         .request<any>({
-            //             url: "/chatModel/getModelList",
-            //             method: "post",
-            //             q_spinning: true,
-            //             data: {},
-            //         })
-            //         .then((res) => {
-            //             if (res.code === 200) {
-            //                 this.modelOptions = res.data.data
-            //                     .filter((e: any) => {
-            //                         return e.id.indexOf("embed") === -1;
-            //                     })
-            //                     .map((e: any) => {
-            //                         return {
-            //                             label: e.id,
-            //                             value: e.id,
-            //                         };
-            //                     });
-            //             }
-            //         }));
+            oneapi === "false" &&
+                (await http
+                    .request<any>({
+                        url: "/chatModel/getModelList",
+                        method: "post",
+                        q_spinning: true,
+                        data: {},
+                    })
+                    .then((res) => {
+                        if (res.code === 200) {
+                            this.modelOptions = res.data.data
+                                .filter((e: any) => {
+                                    return e.id.indexOf("embed") === -1;
+                                })
+                                .map((e: any) => {
+                                    return {
+                                        label: e.id,
+                                        value: e.id,
+                                    };
+                                });
+                        }
+                    }));
 
-            // oneapi === "true" &&
-            await http
-                .request<any>({
-                    url: "/v1/models",
-                    method: "get",
-                    q_baseUrl: process.env.ONEAPI_IP,
-                    q_spinning: true,
-                    q_headers: {
-                        Authorization: process.env.ONEAPI_TOKEN,
-                    },
-                    data: {},
-                })
-                .then((res) => {
-                    this.modelOptions = res.data
-                        .filter((e: any) => {
-                            return e.id.indexOf("embed") === -1;
-                        })
-                        .map((e: any) => {
-                            return {
-                                label: e.id,
-                                value: e.id,
-                            };
-                        });
-                });
+            oneapi === "true" &&
+                (await http
+                    .request<any>({
+                        url: "/v1/models",
+                        method: "get",
+                        q_baseUrl: process.env.ONEAPI_IP,
+                        q_spinning: true,
+                        q_headers: {
+                            Authorization: process.env.ONEAPI_TOKEN,
+                        },
+                        data: {},
+                    })
+                    .then((res) => {
+                        this.modelOptions = res.data
+                            .filter((e: any) => {
+                                return e.id.indexOf("embed") === -1;
+                            })
+                            .map((e: any) => {
+                                return {
+                                    label: e.id,
+                                    value: e.id,
+                                };
+                            });
+                    }));
         },
         getModelOptions() {
             return this.modelOptions;
